997d85444f34342c47244cbfeee54ecc58763256
Roman-pl <romanplohotnuk1@gmail.com>
2021-11-22 18:07:39 +0300

    test func

--- linter.py
+++ linter.py
@@ -1,2 +1,16 @@
-print('Hello world')
-print(2+2)
+s=input('введите строку ')
+def test_check (s):
+	flag = False
+	n=0
+	for i in range(len(s)):
+		n += 1 
+		if s[i] == ' ':
+			if len(s) - n + 1 > 3:
+		 		break
+			else:
+				if s[i]+s[i+1]+s[i+2] == ' + ':
+					flag = True
+				else:
+					flag = False
+	print(flag)				
+test_check(s)
