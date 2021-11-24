da24808137845ed4ab403120e251bb58d27a659a
Roman-pl <romanplohotnuk1@gmail.com>
2021-11-24 18:09:35 +0300

    func +

--- linter.py
+++ linter.py
@@ -1,16 +1,12 @@
-s=input('введите строку ')
+
 def test_check (s):
-	flag = False
-	n=0
-	for i in range(len(s)):
-		n += 1 
-		if s[i] == ' ':
-			if len(s) - n + 1 > 3:
-		 		break
-			else:
-				if s[i]+s[i+1]+s[i+2] == ' + ':
-					flag = True
-				else:
-					flag = False
-	print(flag)				
+	try:
+		if s.rindex(' + ') == True:
+			with open("check_list.txt", "a") as file:
+				print('True',file=file)
+	except ValueError:
+		with open("check_list.txt", "a") as file:
+	 		print('False',file=file)
+s = input('введите строку ')				
 test_check(s)
+
