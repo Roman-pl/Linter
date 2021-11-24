7a17c6db49d74045c399b5f91cd43b2ccb2dd50a
Roman-pl <romanplohotnuk1@gmail.com>
2021-11-24 22:58:29 +0300

    minimum requirements

--- linter.py
+++ linter.py
@@ -1,12 +1,34 @@
+flag = True
+FlagGlobal = True
+def check_plus (s):
+	a = True
+	while s.find('+') >= 1:
+		if s.find(' + ') >= 1:
+			a = True
+			s = s.replace(' + ','')
+		if s.find('+') >= 1:
+			s = s.replace('+','')
+			a = False
+	return a
 
-def test_check (s):
-	try:
-		if s.rindex(' + ') == True:
-			with open("check_list.txt", "a") as file:
-				print('True',file=file)
-	except ValueError:
-		with open("check_list.txt", "a") as file:
-	 		print('False',file=file)
-s = input('введите строку ')				
-test_check(s)
+def check_or (s):
+	a = True
+	while s.find(')or(') >= 1:
+		if s.find(') or (') >= 1:
+			a = True
+			s = s.replace(') or (','')
+		if s.find(')or(') >= 1:
+			s = s.replace(')or(','')
+			a = False
+	return a
 
+with open("example.txt", "r") as file:
+	line = list(file)
+	print(line,file)
+for i in range(len(line)):
+	flag = check_plus(line[i])
+	flag = check_or(line[i])
+	if flag == False:
+		FlagGlobal = False
+with open("check_list.txt", "a") as file:
+    print(FlagGlobal, file=file)
