aa24b8033273756c6fe09e2fbd4e7237834fd3c2
Roman-pl <romanplohotnuk1@gmail.com>
2021-11-25 15:10:41 +0300

    ???

--- linter.py
+++ linter.py
@@ -3,41 +3,28 @@ FlagGlobal = True
 
 def check_plus (s):
 	a = True
-	while s.find('+') >= 1:
-		if s.find(' + ') >= 1:
+	while s.find('+') > -1:
+		if s.find(' + ') > -1:
 			a = True
 			s = s.replace(' + ','')
 		if s.find('+') >= 1:
 			s = s.replace('+','')
 			a = False
-		elif s.find('+ ') >= 1:
-			s = s.replace('+ ','')
-			a = False
-		elif s.find(' +') >= 1:
-			s = s.replace(' +','')
-			a = False
-	return a
 
 def check_equally (s):
 	a = True
-	while s.find('=') >= 1:
+	while s.find('=') > -1:
 		if s.find(' = ') >= 1:
 			a = True
 			s = s.replace(' = ','')
 		if s.find('=') >= 1:
 			s = s.replace('=','')
 			a = False
-		elif s.find('= ') >= 1:
-			s = s.replace('= ','')
-			a = False
-		elif s.find(' =') >= 1:
-			s = s.replace(' =','')
-			a = False
 	return a
 
 def check_or (s):
 	a = True
-	while s.find(')or(') or s.find(')or (') or s.find(') or(') >= 1:
+	while s.find('or') > -1:
 		if s.find(') or (') >= 1:
 			a = True
 			s = s.replace(') or (','')
@@ -54,7 +41,7 @@ def check_or (s):
 
 def check_and (s):
 	a = True
-	while s.find(')and(') or s.find(')and (') or s.find(') and(') >= 1:
+	while s.find(')and(') or s.find(')and (') or s.find(') and(') > -1:
 		if s.find(') and (') >= 1:
 			a = True
 			s = s.replace(') and (','')
@@ -71,46 +58,40 @@ def check_and (s):
 
 def check_more_equally (s):
 	a = True
-	while s.find('>=') >= 1:
+	while s.find('>=') > -1:
 		if s.find(' >= ') >= 1:
 			a = True
 			s = s.replace(' >= ','')
 		if s.find('>=') >= 1:
 			s = s.replace('>=','')
 			a = False
-		elif s.find('>= ') >= 1:
-			s = s.replace('>= ','')
-			a = False
-		elif s.find(' >=') >= 1:
-			s = s.replace(' >=','')
-			a = False
 	return a
 
 def check_more_equally (s):
 	a = True
-	while s.find('<=') >= 1:
+	while s.find('<=') > -1:
 		if s.find(' <= ') >= 1:
 			a = True
 			s = s.replace(' <= ','')
 		if s.find('<=') >= 1:
 			s = s.replace('<=','')
 			a = False
-		elif s.find('<= ') >= 1:
-			s = s.replace('<= ','')
-			a = False
-		elif s.find(' <=') >= 1:
-			s = s.replace(' <=','')
-			a = False
+		return a
 	return a
 
 with open("example.txt", "r") as file:
 	line = list(file)
 for i in range(len(line)):
-	flag = check_plus((line[i]))
-	flag = check_or(line[i])
-	flag = check_and(line[i])
-	flag = check_more_equally(line[i])
-	flag = check_less_equally(line[i])
+	if line[i].find('+') > -1:
+		flag = check_plus((line[i]))
+	elif line[i].find('<=') > -1:
+		flag = check_less_equally(line[i])
+	elif line[i].find('>=') > -1:
+		flag = check_more_equally(line[i])
+	elif line[i].find(')or(') or line[i].find(') or(') or  line[i].find(')or (') > -1:
+		flag = check_or(line[i])
+	else:
+		flag = check_and(line[i])
 	if flag == False:
 		FlagGlobal = False
 with open("check_list.txt", "a") as file:
