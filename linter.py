55898970b0ca2ccde12cc571f349b4e8472788f7
Roman-pl <romanplohotnuk1@gmail.com>
2021-11-27 17:13:00 +0300

    bug fix check + and =

--- linter.py
+++ linter.py
@@ -2,53 +2,72 @@ flag = True
 FlagGlobal = True
 
 def check_plus(s):
-	for i in range(len(s)):
-		if s[i] == '+':
-			if s[i]+s[i+1] == '+ ' and s[i-1]+s[i] == ' +' and s[i+2] != ' ' and s[i-2] != ' ':
-				return True
+	flag2 = True
+	s2 = ' '.join(s.split(' '))
+	if s != s2:
+		flag2 = False
+	for i in range(len(s2)):
+		if s2[i] == '+':
+			if s2[i]+s2[i+1] == '+ ' and s2[i-1]+s2[i] == ' +' and s2[i+2] != ' ' and s2[i-2] != ' ':
+				flag = True
 			else:
-				return False
+				flag = False
+	if flag2 == True and flag == True:
+		return True
+	else:
+		return False
 
-def check_more(s):
+# def check_more(s):
 	
-
-def check_less(s):
+# def check_less(s):
 	
 
 def check_mr_eq(s):
-	for i in range(len(s)):
-		if s[i]+s[i+1] == '>=':
-			if s[i]+s[i+1]+s[i+2] == '>= ' and s[i-2]+s[i-1]+s[i] == ' >=' and s[i+3] != ' ' and s[i-3] != ' ':
-				return True
-			else:
-				return False
+	flag2 = True
+	s2 = ' '.join(s.split(' '))
+	if s != s2:
+		flag2 = False
+		
+	if flag2 == True and flag == True:
+		return True
+	else:
+		return False
 
 def check_ls_eq(s):
-	for i in range(len(s)):
-		if s[i]+s[i+1] == '<=':
-			if s[i]+s[i+1]+s[i+3] == '<= ' and s[i-2]+s[i-1]+s[i] == ' <=' and s[i+3] != ' ' and s[i-3] != ' ':
-				return True
-			else:
-				return False
+	flag2 = True
+	s2 = ' '.join(s.split(' '))
+	if s != s2:
+		flag2 = False
+
+	if flag2 == True and flag == True:
+		return True
+	else:
+		return False
 
 def check_eq(s):
+	flag = True
+	flag2 = True
 	s = s.replace('>=','')
 	s = s.replace('<=','')
-	for i in range(len(s)):
-		if s[i] = '=':
+	s2 = ' '.join(s.split(' '))
+	if s != s2:
+		flag2 = False
+	for i in range(len(s2)):
+		if s[i] == '=':
 			if s[i]+s[i+1] == '= ' and s[i-1]+s[i] == ' =' and s[i+2] != ' ' and s[i-2] != ' ':
-				return True
+				flag = True
 			else:
-				return False
+				flag = False
+	if flag2 == True and flag == True:
+		return True
+	else:
+		return False
 
 with open("example.txt", "r") as file:
 	line = list(file)
 for i in range(len(line)):
 	if line[i].find('+'):
-		if check_plus(s) == False:
-			FlagGlobal = False
-	if line[i].find('>'):
-		if check_more(s) == False:
+		if check_plus(line[i]) == False:
 			FlagGlobal = False
 
 with open("check_list.txt", "a") as file:
