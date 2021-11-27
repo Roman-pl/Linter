35252dcf6c6ee7125c8ca1cf98f5fbffaa161c05
Roman-pl <romanplohotnuk1@gmail.com>
2021-11-27 16:26:08 +0300

    bug fix check

--- linter.py
+++ linter.py
@@ -3,24 +3,40 @@ FlagGlobal = True
 
 def check_plus(s):
 	for i in range(len(s)):
-		if s[i] = '+':
+		if s[i] == '+':
 			if s[i]+s[i+1] == '+ ' and s[i-1]+s[i] == ' +' and s[i+2] != ' ' and s[i-2] != ' ':
 				return True
 			else:
 				return False
 
 def check_more(s):
+	
+
+def check_less(s):
+	
+
+def check_mr_eq(s):
 	for i in range(len(s)):
-		if s[i] = '>':
-			if s[i]+s[i+1] == '> ' and s[i-1]+s[i] == ' >' and s[i+2] != ' ' and s[i-2] != ' ':
+		if s[i]+s[i+1] == '>=':
+			if s[i]+s[i+1]+s[i+2] == '>= ' and s[i-2]+s[i-1]+s[i] == ' >=' and s[i+3] != ' ' and s[i-3] != ' ':
 				return True
 			else:
 				return False
 
-def check_less(s):
+def check_ls_eq(s):
 	for i in range(len(s)):
-		if s[i] = '<':
-			if s[i]+s[i+1] == '< ' and s[i-1]+s[i] == ' <' and s[i+2] != ' ' and s[i-2] != ' ':
+		if s[i]+s[i+1] == '<=':
+			if s[i]+s[i+1]+s[i+3] == '<= ' and s[i-2]+s[i-1]+s[i] == ' <=' and s[i+3] != ' ' and s[i-3] != ' ':
+				return True
+			else:
+				return False
+
+def check_eq(s):
+	s = s.replace('>=','')
+	s = s.replace('<=','')
+	for i in range(len(s)):
+		if s[i] = '=':
+			if s[i]+s[i+1] == '= ' and s[i-1]+s[i] == ' =' and s[i+2] != ' ' and s[i-2] != ' ':
 				return True
 			else:
 				return False
